c76997e99e159412c6271fca7e40517b40312803
AhasnaCharumee <charumeeahasna@gmail.com>
2026-04-01 09:50:41 +0530

    first commit

---
+++ src/main/java/lk/ijse/agms/AgmsApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.agms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgmsApplication.class, args);
+	}
+
+}
