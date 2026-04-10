357f8da1d91d51a44860d17c49742e06dd9b18da
AhasnaCharumee <charumeeahasna@gmail.com>
2026-04-10 11:24:18 +0530

    fix(automation-service): align application name and config import with Config Server

---
+++ automation-control-service/src/main/java/lk/ijse/automationcontrolservice/controller/AutomationController.java
@@ -0,0 +1,31 @@
+package lk.ijse.automationcontrolservice.controller;
+
+import lk.ijse.automationcontrol.dto.SensorData;
+import lk.ijse.automationcontrol.model.ActionLog;
+import lk.ijse.automationcontrol.service.AutomationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/automation")
+public class AutomationController {
+
+    private final AutomationService automationService;
+
+    public AutomationController(AutomationService automationService) {
+        this.automationService = automationService;
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<Void> receiveSensorData(@RequestBody SensorData sensorData) {
+        automationService.processSensorData(sensorData);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/logs")
+    public ResponseEntity<List<ActionLog>> getAutomationLogs() {
+        return ResponseEntity.ok(automationService.getAllLogs());
+    }
+}
